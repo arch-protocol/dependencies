5e4b2a1acacfe11aed55fe4dfb36acec44a77456
Gaonuk <rodrigogaonagonzalez@gmail.com>
2023-07-31 18:34:06 -0400

    ✨ Add uniswap interfaces and libraries

---
+++ src/uniswap/interfaces/IERC721Permit.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity ^0.8.17;
+
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+
+/// @title ERC721 with permit
+/// @notice Extension to ERC721 that includes a permit function for signature based approvals
+interface IERC721Permit is IERC721 {
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function permit(address spender, uint256 tokenId, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external
+        payable;
+}
