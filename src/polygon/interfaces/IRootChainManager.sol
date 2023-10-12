66fa6c44064ce2721a34cea3cff2e3c605dfdf5d
Gaonuk <rodrigogaonagonzalez@gmail.com>
2023-10-12 11:36:22 -0300

    ✨ Add exit function to rootChainManager

--- src/polygon/interfaces/IRootChainManager.sol
+++ src/polygon/interfaces/IRootChainManager.sol
@@ -5,4 +5,6 @@ interface IRootChainManager {
     function depositFor(address user, address rootToken, bytes calldata depositData) external;
 
     function depositEtherFor(address user) external payable;
+
+    function exit(bytes calldata inputData) external;
 }
